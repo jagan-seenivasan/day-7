3486b312015b9554fe6e5fd408b93fe6670a486e
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2021-12-27 21:38:01 +0530

    Add files via upload

---
+++ prblm1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x 
+
+MAXCOUNT=10
+count=1
+
+while [ "$count" -le $MAXCOUNT ]; do
+ number[$count]=$((RANDOM%100+900))
+ let "count += 1"
+done
+
+
+echo "${number[@]}"
+
+array = $number
+
+if [ "${#array[@]}" -lt 2 ]
+then
+  echo Incoming array is not large enough >&2
+  exit 1
+fi
+
+largest=${array[0]}
+secondGreatest='unset'
+
+for((i=1; i < ${#array[@]}; i++))
+do
+  if [[ ${array[i]} > $largest ]]
+  then
+    secondGreatest=$largest
+    largest=${array[i]}
+  elif (( ${array[i]} != $largest )) && { [[ "$secondGreatest" = "unset" ]] || [[ ${array[i]} > $secondGreatest ]]; }
+  then
+    secondGreatest=${array[i]}
+  fi
+done
+
+echo "secondGreatest = $secondGreatest"
